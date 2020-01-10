b5fb3fc62c19e60de04aec83aa595fe54cd10fb4
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-01-10 14:38:35 -0600

    Fetch data using fetch -> Isomorphic fetch and getInitialProps()

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import { Component } from 'react'
 import Link from 'next/link'
 import Layout from '../components/layout'
+import fetch from 'isomorphic-unfetch'
 
-const About = () => {
-    return (
-        <Layout title="About">
-            <Link href="/">
-                <a>Homepage</a>
-            </Link>
-            <h2>JavaScript Programmer</h2>
-            <img src="https://i.ytimg.com/vi/Fnw3lNeH-XI/maxresdefault.jpg" alt="Next JS" height="200px" />
-        </Layout>
-    )
+export default class About extends Component {
+    static async getInitialProps() {
+        // fetch('https://api.github.com/users/guhandelta')
+        //     .then(res => res.json)
+        //     .then(data => console.log(data));
+
+        const resp = await fetch('https://api.github.com/users/guhandelta');
+        const data = await resp.json();
+
+        return { user: data };
+    }
+    //The req for data may also be given in a componentDidMount(), but since this is server rendered app, getInitialProps()-
+    //- would be a good choice, though componentDidMount() can still be used => user data will be stored in a state variable
+
+    render() {
+        const { user } = this.props;
+        return (
+            <Layout title="About">
+                <h5>{user.name}</h5>
+                <img src={user.avatar_url} alt="Something's wrong" height="200px" />
+            </Layout>
+        )
+    }
 }
-export default About;
